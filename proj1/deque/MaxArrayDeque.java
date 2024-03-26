06cf5f708c89a463f8ea00e98dc8207d9852cbd5
travel2love <irelia2@qq.com>
2024-03-26 14:17:12 +0800

    with proj1

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,2 +1,15 @@
-package deque;public class MaxArrayDeque {
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> {
+    public MaxArrayDeque(Comparator<T> c){
+
+    }
+    public T max(){
+        return null;
+    }
+    public T max(Comparator<T> c){
+        return null;
+    }
 }
